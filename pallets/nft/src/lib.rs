125e08478fb1dfa58bbf495e636250df2a16c831
Dan Forbes <dan@danforbes.dev>
2020-06-22 20:23:41 -0700

    Make NFT pallet instantiable

--- pallets/nft/src/lib.rs
+++ pallets/nft/src/lib.rs
@@ -12,13 +12,13 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-pub trait Trait: system::Trait {
+pub trait Trait<I = DefaultInstance>: system::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
     type AssetInfo: Hashable + Member + MaybeSerialize + Debug + Default + FullCodec;
 }
 
 decl_storage! {
-    trait Store for Module<T: Trait> as NFT {
+    trait Store for Module<T: Trait<I>, I: Instance = DefaultInstance> as NFT {
         // Mapping from holder address to their (enumerable) set of owned assets
         AssetsForAccount get(fn assets_for_account): map hasher(blake2_128_concat) T::AccountId => Vec<Vec<u8>>;
         // Mapping from asset ID to the address that owns it
@@ -29,7 +29,7 @@ decl_storage! {
 }
 
 decl_event!(
-    pub enum Event<T>
+    pub enum Event<T, I = DefaultInstance>
     where
         AccountId = <T as system::Trait>::AccountId,
     {
@@ -38,7 +38,7 @@ decl_event!(
 );
 
 decl_error! {
-    pub enum Error for Module<T: Trait> {
+    pub enum Error for Module<T: Trait<I>, I: Instance> {
         // The asset already exists
         AssetExists,
     }
@@ -46,8 +46,8 @@ decl_error! {
 
 // The pallet's dispatchable functions.
 decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        type Error = Error<T>;
+    pub struct Module<T: Trait<I>, I: Instance = DefaultInstance> for enum Call where origin: T::Origin {
+        type Error = Error<T, I>;
         fn deposit_event() = default;
 
         #[weight = 10_000]
@@ -55,13 +55,13 @@ decl_module! {
             let who = ensure_signed(origin)?;
             let asset_info = T::AssetInfo::default();
             let asset_id = asset_info.blake2_128_concat();
-            if InfoForAsset::<T>::contains_key(&asset_id) {
-                Err(Error::<T>::AssetExists)?;
+            if InfoForAsset::<T, I>::contains_key(&asset_id) {
+                Err(Error::<T, I>::AssetExists)?;
             }
 
-            AssetsForAccount::<T>::append(&who, &asset_id);
-            AccountForAsset::<T>::insert(&asset_id, &who);
-            InfoForAsset::<T>::insert(&asset_id, asset_info);
+            AssetsForAccount::<T, I>::append(&who, &asset_id);
+            AccountForAsset::<T, I>::insert(&asset_id, &who);
+            InfoForAsset::<T, I>::insert(&asset_id, asset_info);
             Self::deposit_event(RawEvent::AssetMinted(asset_id, who));
             Ok(())
         }
