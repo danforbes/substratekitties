732eec4d5f83a15143b47dfdcd5a98029e7d8f3e
Dan Forbes <dan@danforbes.dev>
2020-08-13 12:25:55 -0700

    Clean up code

--- pallets/substratekitties/src/lib.rs
+++ pallets/substratekitties/src/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_module, decl_event, decl_error, dispatch};
-use frame_system::{self as system, ensure_signed};
+use frame_support::{decl_module, decl_event, decl_error};
 
 #[cfg(test)]
 mod mock;
@@ -9,12 +8,12 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-pub trait Trait: system::Trait {
-	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+pub trait Trait: frame_system::Trait {
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
 decl_event!(
-	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
 		KittyClaimed(AccountId),
 	}
 );
@@ -29,11 +28,5 @@ decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		type Error = Error<T>;
 		fn deposit_event() = default;
-
-		#[weight = 10_000]
-		pub fn claim_kitty(origin) -> dispatch::DispatchResult {
-			ensure_signed(origin)?;
-			Ok(())
-		}
 	}
 }
