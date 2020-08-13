55fbd93d1123f96d06ee73915170a1a43ae553f2
Dan Forbes <dan@danforbes.dev>
2020-08-13 12:28:20 -0700

    TODOs to describe use cases

--- pallets/substratekitties/src/lib.rs
+++ pallets/substratekitties/src/lib.rs
@@ -28,5 +28,29 @@ decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		type Error = Error<T>;
 		fn deposit_event() = default;
+
+		// TODO: DEFINE
+		// kitty = {dna, power}
+
+		// TODO: CREATE
+		// create a kitty with power proportional to some locked funds
+		// kitty is assigned to account that dispatched call
+		// DNA should be random, initial power derived from DNA
+		// DNA used to derive avatar https://www.peppercarrot.com/extras/html/2016_cat-generator/
+		// total fee = base fee + optional power multiplier fee
+
+		// TODO: BOOST
+		// power up a kitty by locking more funds
+		// increases power without altering DNA
+
+		// TODO: ADVERTISE
+		// post intent to breed
+
+		// TODO: BREED
+		// respond to intent to breed
+		// may not result in offspring
+		// DNA and power derived from parents
+		// each parent randomly "contributes" power
+		// offspring owner randomly assigned between parent owners
 	}
 }
