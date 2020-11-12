bab8b37ed344c5ad436defb8693b621a25f25eac
goldsteinsveta <sveta@parity.io>
2020-11-12 15:24:02 +0100

    theme data

---
+++ front-end/src/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  colors: {
+    app: {
+      bg: '#FAFAFA',
+      highlight: '#FFFFFF',
+      input: '',
+      text: '#000000',
+      border: '#DFDFDF'
+    },
+    console: {
+      bg: '#1F1726',
+      highlight: '#403453',
+      input: '#09502E',
+      text: '#6AFF79',
+      border: '#00B488'
+    },
+    brand: {
+      bg: 'linear-gradient(90deg, #7164C6 0%, #6C98F5 20.84%, #7DD3D8 40.05%, #D5ECCE 74.9%, #E2C3D5 100%)'
+    }
+  }
+};
+
+export default theme;
